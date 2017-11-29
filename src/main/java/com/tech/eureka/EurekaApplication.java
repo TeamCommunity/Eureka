6df90687897e72bac6d3cb68b130f2ed37a0e39a
Taketo Matsunaga <taketom@jp.ibm.com>
2017-11-29 15:38:38 +0900

    first commit

---
+++ src/main/java/com/tech/eureka/EurekaApplication.java
@@ -0,0 +1,24 @@
+package com.tech.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication extends SpringBootServletInitializer {
+
+	private static Class<EurekaApplication> applicationClass = EurekaApplication.class;
+
+	public static void main(String[] args) {
+		SpringApplication.run(applicationClass, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+}
